a919588c9bbb7d8f4abcad2d3fd036607217419c
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2024-10-18 22:26:04 +0400

    f

--- src/Appw.jsx
+++ src/Appw.jsx
@@ -4,12 +4,15 @@ import withMoney from './withMoney';
 import Person1 from './components/Person1';
 import Person2 from './components/Person2';
 
-const adaminifulit = withMoney(person1);
+const UbgradeMoney1 = withMoney(Person1);
+const UbgradeMoney2 = withMoney(Person2);
 
 const Appw = () => {
   return (
     <div>
       <h1>HOC</h1>
+      <UbgradeMoney1 />
+      <UbgradeMoney2 />
     </div>
   );
 };
